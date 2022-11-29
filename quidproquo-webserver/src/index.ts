8422a0161bacbbd7bc87f80a1171231daa0ccac6
joe-coady <joecoady@gmail.com>
2022-11-29 15:40:45 +1000

    exported missing type defs

--- quidproquo-webserver/src/index.ts
+++ quidproquo-webserver/src/index.ts
@@ -7,4 +7,8 @@ export {
   ServiceInfrastructureConfigType,
 } from "./serviceInfrastructureDefinitions/ServiceInfrastructureConfig";
 
+export { DnsServiceInfrastructureConfig } from "./serviceInfrastructureDefinitions/dns";
+export { RouteInfrastructureConfig } from "./serviceInfrastructureDefinitions/route";
+export { ServiceNameInfrastructureConfig } from "./serviceInfrastructureDefinitions/serviceName";
+
 export { default as utils } from "./utils";
