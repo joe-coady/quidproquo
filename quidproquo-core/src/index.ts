f1fb230e79c17fc87a8c5ccf63ba9f2e4f99a3e7
joe-coady <joecoady@gmail.com>
2022-11-30 22:42:32 +1000

    exposed resolveStory

--- quidproquo-core/src/index.ts
+++ quidproquo-core/src/index.ts
@@ -1,19 +1,14 @@
-export { DateActionTypeEnum } from "./actions/date/DateActionTypeEnum";
-export { GuidActionTypeEnum } from "./actions/guid/GuidActionTypeEnum";
-export { EventActionTypeEnum } from "./actions/event/EventActionTypeEnum";
-export { MathActionTypeEnum } from "./actions/math/MathActionTypeEnum";
-export { PlatformActionTypeEnum } from "./actions/platform/PlatformActionTypeEnum";
-export { SystemActionTypeEnum } from "./actions/system/SystemActionTypeEnum";
+export { DateActionTypeEnum } from './actions/date/DateActionTypeEnum';
+export { GuidActionTypeEnum } from './actions/guid/GuidActionTypeEnum';
+export { EventActionTypeEnum } from './actions/event/EventActionTypeEnum';
+export { MathActionTypeEnum } from './actions/math/MathActionTypeEnum';
+export { PlatformActionTypeEnum } from './actions/platform/PlatformActionTypeEnum';
+export { SystemActionTypeEnum } from './actions/system/SystemActionTypeEnum';
 
-export {
-  defineAppName,
-  AppNameQPQConfigSetting,
-} from "./config/settings/appName";
+export { defineAppName, AppNameQPQConfigSetting } from './config/settings/appName';
 
-export {
-  QPQCoreConfigSettingType,
-  QPQConfigSetting,
-  QPQConfig,
-} from "./config/QPQConfig";
+export { QPQCoreConfigSettingType, QPQConfigSetting, QPQConfig } from './config/QPQConfig';
 
-export * as qpqCoreUtils from "./qpqCoreUtils";
+export * as qpqCoreUtils from './qpqCoreUtils';
+
+export { resolveStory } from './actionProcessor';
