b71500a4207eb5319e66da9c1c6cdd86924a262d
joe-coady <joecoady@gmail.com>
2022-12-07 17:14:22 +1000

    qpqCoreUtil: getStorageDriveNames

--- quidproquo-core/src/qpqCoreUtils.ts
+++ quidproquo-core/src/qpqCoreUtils.ts
@@ -1,39 +1,39 @@
-import {
-  QPQConfig,
-  QPQConfigSetting,
-  QPQCoreConfigSettingType,
-} from "./config/QPQConfig";
-import { AppNameQPQConfigSetting } from "./config/settings/appName";
+import { QPQConfig, QPQConfigSetting, QPQCoreConfigSettingType } from './config/QPQConfig';
+import { AppNameQPQConfigSetting, StorageDriveQPQConfigSetting } from './config/settings';
 
 export const getConfigSettings = <T extends QPQConfigSetting>(
   configs: QPQConfig,
-  configSettingType: string
+  configSettingType: string,
 ): T[] => {
-  return configs.filter(
-    (c) => c.configSettingType === configSettingType
-  ) as T[];
+  return configs.filter((c) => c.configSettingType === configSettingType) as T[];
 };
 
 export const getConfigSetting = <T extends QPQConfigSetting>(
   configs: QPQConfig,
-  serviceInfrastructureConfigType: string
+  serviceInfrastructureConfigType: string,
 ): T | undefined => {
-  const [setting] = getConfigSettings<T>(
-    configs,
-    serviceInfrastructureConfigType
-  );
+  const [setting] = getConfigSettings<T>(configs, serviceInfrastructureConfigType);
   return setting;
 };
 
 export const getAppName = (configs: QPQConfig): string => {
   const appName = getConfigSetting<AppNameQPQConfigSetting>(
     configs,
-    QPQCoreConfigSettingType.appName
+    QPQCoreConfigSettingType.appName,
   )?.appName;
 
   if (!appName) {
-    throw new Error("please use defineAppName in your QPQ config");
+    throw new Error('please use defineAppName in your QPQ config');
   }
 
   return appName;
+};
+
+export const getStorageDriveNames = (configs: QPQConfig): string[] => {
+  const storageDriveNames = getConfigSettings<StorageDriveQPQConfigSetting>(
+    configs,
+    QPQCoreConfigSettingType.storageDrive,
+  ).map((sd) => sd.storageDrive);
+
+  return storageDriveNames;
 };
