246506d3466ef032c912971ba282914fe0ffb97e
joe-coady <joecoady@gmail.com>
2023-01-19 15:00:18 +1000

    Construct base

---
+++ quidproquo-deploy-awscdk/src/constructs/core/QpqConstruct.ts
@@ -0,0 +1,39 @@
+import { QPQConfig, QPQConfigSetting, qpqCoreUtils } from 'quidproquo-core';
+import { Construct } from 'constructs';
+
+export interface QpqConstructProps<T extends QPQConfigSetting> {
+  setting: T;
+  qpqConfig: QPQConfig;
+}
+
+export class QpqConstruct<T extends QPQConfigSetting> extends Construct {
+  qpqConfig: QPQConfig;
+  id: string;
+
+  constructor(scope: Construct, id: string, props: QpqConstructProps<T>) {
+    super(scope, id);
+
+    this.qpqConfig = props.qpqConfig;
+    this.id = id;
+  }
+
+  environment() {
+    return qpqCoreUtils.getAppFeature(this.qpqConfig);
+  }
+
+  service() {
+    return qpqCoreUtils.getAppName(this.qpqConfig);
+  }
+
+  deploymentPrefix() {
+    return `${this.service()}-${this.environment()}`;
+  }
+
+  resourceName(name: string, maxLength: number = 60) {
+    return `${name}-${this.service()}-${this.environment()}`;
+  }
+
+  childId(uniqueName: string) {
+    return `${uniqueName}-${this.id}`;
+  }
+}
