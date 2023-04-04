654778a76d40bef3efbeab88e72275a1ce26fd77
joe-coady <joecoady@gmail.com>
2023-04-04 12:21:11 +1000

    Added OpenID Connect specification user attributes

--- quidproquo-actionprocessor-awslambda/src/logic/cognito/createUser.ts
+++ quidproquo-actionprocessor-awslambda/src/logic/cognito/createUser.ts
@@ -10,6 +10,37 @@ import {
 import { authenticateUser } from './authenticateUser';
 import { setUserPassword } from './setUserPassword';
 
+const cognitoAttributeMap: Record<keyof CreateUserRequest, string> = {
+  email: 'email',
+  emailVerified: 'email_verified',
+  password: 'password',
+
+  address: 'address',
+  birthDate: 'birthdate',
+  familyName: 'family_name',
+  gender: 'gender',
+  givenName: 'given_name',
+  locale: 'locale',
+  middleName: 'middle_name',
+  name: 'name',
+  nickname: 'nickname',
+  phoneNumber: 'phone_number',
+  picture: 'picture',
+  preferredUsername: 'preferred_username',
+  profile: 'profile',
+  website: 'website',
+  zoneInfo: 'zoneinfo',
+};
+
+export const getUserAttributesFromCreateUserRequest = (createUserRequest: CreateUserRequest) => {
+  return Object.keys(createUserRequest)
+    .map((key) => ({
+      Name: cognitoAttributeMap[key as keyof CreateUserRequest],
+      Value: `${createUserRequest[key as keyof CreateUserRequest]}`,
+    }))
+    .filter((attribute) => !!attribute.Value && attribute.Name !== 'password');
+};
+
 export const createUser = async (
   userPoolId: string,
   region: string,
@@ -23,16 +54,12 @@ export const createUser = async (
     Username: createUserRequest.email,
     MessageAction: MessageActionType.SUPPRESS, // Don't contact the user
     DesiredDeliveryMediums: [DeliveryMediumType.EMAIL],
-    UserAttributes: [
-      { Name: 'email', Value: createUserRequest.email },
-      { Name: 'email_verified', Value: 'false' },
-    ],
+    UserAttributes: getUserAttributesFromCreateUserRequest(createUserRequest),
     ForceAliasCreation: false,
   };
 
-  if (createUserRequest.phone) {
+  if (createUserRequest.phoneNumber) {
     params.DesiredDeliveryMediums!.push(DeliveryMediumType.SMS);
-    params.UserAttributes!.push({ Name: 'phone_number', Value: createUserRequest.phone });
   }
 
   const response = await cognitoClient.send(new AdminCreateUserCommand(params));
