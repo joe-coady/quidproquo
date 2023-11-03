c8c7882fbb3ef39312d5f40dc655605144a18fda
joecoady <joecoady@gmail.com>
2023-11-03 15:54:08 +1000

    Bug fix, query strings and removal of production in domain redirect

--- quidproquo-deploy-awscdk/src/lambdas/lambdaAPIGatewayEvent_redirect.ts
+++ quidproquo-deploy-awscdk/src/lambdas/lambdaAPIGatewayEvent_redirect.ts
@@ -2,8 +2,8 @@ import { APIGatewayEvent, Context } from 'aws-lambda';
 import { SubdomainRedirectQPQWebServerConfigSetting } from 'quidproquo-webserver';
 
 export const executeAPIGatewayEvent = async (event: APIGatewayEvent, context: Context) => {
-  // console.log('event: ', JSON.stringify(event, null, 2));
-  // console.log('process.env: ', JSON.stringify(process.env, null, 2));
+  console.log('event: ', JSON.stringify(event, null, 2));
+  console.log('process.env: ', JSON.stringify(process.env, null, 2));
 
   const redirectConfig: SubdomainRedirectQPQWebServerConfigSetting = JSON.parse(
     process.env.redirectConfig as string,
@@ -14,23 +14,32 @@ export const executeAPIGatewayEvent = async (event: APIGatewayEvent, context: Co
 
   // Otherwise it must be a domain redirect
   if (!redirectConfig.redirectUrl.startsWith('http')) {
-    const environment: SubdomainRedirectQPQWebServerConfigSetting = JSON.parse(
-      process.env.environment as string,
-    );
+    const environment: string = JSON.parse(process.env.environment as string);
 
-    const featureEnvironment: SubdomainRedirectQPQWebServerConfigSetting = JSON.parse(
+    const featureEnvironment: string = JSON.parse(
       (process.env.featureEnvironment as string | undefined) || '""',
     );
 
     let baseDomain = redirectConfig.redirectUrl;
-    if (redirectConfig.addEnvironment) {
+    if (redirectConfig.addEnvironment && environment !== 'production') {
       baseDomain = `${environment}.${baseDomain}`;
     }
+
     if (redirectConfig.addFeatureEnvironment && featureEnvironment) {
       baseDomain = `${featureEnvironment}.${baseDomain}`;
     }
 
     redirectUrl = `https://${baseDomain}${event.path}`;
+  }
+
+  // Add query parameters if they exist
+  const queryParams = event.queryStringParameters;
+  if (queryParams) {
+    const queryString = Object.entries(queryParams)
+      .filter(([, value]) => value !== undefined) // Filter out undefined values
+      .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value as string)}`)
+      .join('&');
+    redirectUrl += `?${queryString}`;
   }
 
   return {
