896fdcfdc27ce1ec829b2aeca3cd2a3693c7fcfd
joecoady <joecoady@gmail.com>
2023-11-08 10:47:05 +1000

    added page support to askCloudflareGetDNSRecordId

--- quidproquo-webserver/src/services/cloudflare/logic/askCloudflareGetDNSRecordId.ts
+++ quidproquo-webserver/src/services/cloudflare/logic/askCloudflareGetDNSRecordId.ts
@@ -6,25 +6,38 @@ export function* askCloudflareGetDNSRecordId(
   zoneId: string,
   cnameName: string,
 ): AskResponse<string | undefined> {
-  const response = yield* askNetworkRequest<void, CloudflareResponse<CloudflareDNSRecord>>(
-    'GET',
-    `https://api.cloudflare.com/client/v4/zones/${zoneId}/dns_records?name=${cnameName}`,
-    {
-      headers: {
-        Authorization: `Bearer ${apiKey}`,
-        'Content-Type': 'application/json',
+  let page = 1;
+  let total_pages = 0;
+  let allRecords: CloudflareDNSRecord[] = [];
+
+  do {
+    const response = yield* askNetworkRequest<void, CloudflareResponse<CloudflareDNSRecord>>(
+      'GET',
+      `https://api.cloudflare.com/client/v4/zones/${zoneId}/dns_records?name=${cnameName}&page=${page}`,
+      {
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+          'Content-Type': 'application/json',
+        },
       },
-    },
-  );
+    );
 
-  if (response.status < 200 || response.status >= 300) {
-    yield* askThrowError(ErrorTypeEnum.GenericError, `Error getting DNS record for [${cnameName}]`);
-  }
+    if (response.status < 200 || response.status >= 300) {
+      yield* askThrowError(
+        ErrorTypeEnum.GenericError,
+        `Error getting DNS record for [${cnameName}]`,
+      );
+    }
 
-  // Filter out all the non-cname records
-  const filteredRecords = response.data.result.filter(
-    (record) => record.type === 'CNAME' || record.type === 'A',
-  );
+    // Filter out all the non-CNAME and non-A records from the combined results
+    const filteredRecords = response.data.result.filter(
+      (record) => record.type === 'CNAME' || record.type === 'A',
+    );
 
-  return filteredRecords[0]?.id;
+    allRecords = [...allRecords, ...filteredRecords];
+    total_pages = response.data.result_info.total_pages;
+    page += 1;
+  } while (page <= total_pages);
+
+  return allRecords[0]?.id;
 }
