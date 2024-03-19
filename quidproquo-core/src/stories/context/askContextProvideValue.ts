6d6df49287c35a11a7c11660a1b89b9b7a752897
joecoady <joecoady@gmail.com>
2024-03-19 20:01:48 +1000

    bug fix: askContextProvideValue return value

--- quidproquo-core/src/stories/context/askContextProvideValue.ts
+++ quidproquo-core/src/stories/context/askContextProvideValue.ts
@@ -1,72 +1,76 @@
-import { ContextActionType, ContextReadActionPayload } from "../../actions/context";
-import { 
-    Action,
-    AskResponse, 
-    AskResponseReturnType, 
-    EitherActionResult, 
-    QpqContext, 
-    QpqContextIdentifier
-  } from "../../types";
-
+import { ContextActionType, ContextReadActionPayload } from '../../actions/context';
+import {
+  Action,
+  AskResponse,
+  AskResponseReturnType,
+  EitherActionResult,
+  QpqContext,
+  QpqContextIdentifier,
+} from '../../types';
 
 export function* askContextProvideValue<R, T extends AskResponse<any>>(
-    contextIdentifier: QpqContextIdentifier<R>,
-    value: R,
-    storyIterator: T
-  ): AskResponse<EitherActionResult<AskResponseReturnType<T>>> {
-    let nextResult = storyIterator.next();
+  contextIdentifier: QpqContextIdentifier<R>,
+  value: R,
+  storyIterator: T,
+): AskResponse<AskResponseReturnType<T>> {
+  let nextResult = storyIterator.next();
 
-    // We cache the context values because the parent can't change, unilateral dataflow
-    // and we don't want to recompute the context values every time we are asked for them.
-    // we dont want to hit the hit the owner of the context as it shows in the logs for no reason
-    let cache: QpqContext<any> | null = null;
+  // We cache the context values because the parent can't change, unilateral dataflow
+  // and we don't want to recompute the context values every time we are asked for them.
+  // we dont want to hit the hit the owner of the context as it shows in the logs for no reason
+  let cache: QpqContext<any> | null = null;
 
-    while (!nextResult.done) {
-        // If this action is a read context
-        if (nextResult.value.type === ContextActionType.Read) {
-            // and its trying to read from this context
-            const contextActionItterator = nextResult as IteratorYieldResult<Action<ContextReadActionPayload<any>>>;
-            if (contextActionItterator.value.payload!.contextIdentifier.uniqueName === contextIdentifier.uniqueName) {
-                // then we feed it our value
-                nextResult = storyIterator.next(value);
+  while (!nextResult.done) {
+    // If this action is a read context
+    if (nextResult.value.type === ContextActionType.Read) {
+      // and its trying to read from this context
+      const contextActionItterator = nextResult as IteratorYieldResult<
+        Action<ContextReadActionPayload<any>>
+      >;
+      if (
+        contextActionItterator.value.payload!.contextIdentifier.uniqueName ===
+        contextIdentifier.uniqueName
+      ) {
+        // then we feed it our value
+        nextResult = storyIterator.next(value);
 
-                // And keep processing
-                continue;
-            }
-        }
-
-        // If we are trying to list all context values
-        else if (nextResult.value.type === ContextActionType.List) {
-            // Update the cache
-            if (cache === null) {
-                // Grab the parent context values
-                const parentContextValues = yield nextResult.value;
-
-                // overide / attach our context value
-                const allContextValues = {
-                    ...parentContextValues,
-                    [contextIdentifier.uniqueName]: value
-                }
-
-                // Update the cache
-                cache = allContextValues;
-            }
-            
-            // pass in our chached context values
-            nextResult = storyIterator.next(cache);
-
-            // And keep processing
-            continue;
-        }
-
-        // Otherwise this is not a context action
-        // use the parent processor to process it.
-        const actionValue = yield nextResult.value;
-        
-        // and pass that value down to our children.
-        nextResult = storyIterator.next(actionValue);
+        // And keep processing
+        continue;
+      }
     }
 
-    // Return the successful final result of the generator
-    return nextResult.value;
+    // If we are trying to list all context values
+    else if (nextResult.value.type === ContextActionType.List) {
+      // Update the cache
+      if (cache === null) {
+        // Grab the parent context values
+        const parentContextValues = yield nextResult.value;
+
+        // overide / attach our context value
+        const allContextValues = {
+          ...parentContextValues,
+          [contextIdentifier.uniqueName]: value,
+        };
+
+        // Update the cache
+        cache = allContextValues;
+      }
+
+      // pass in our chached context values
+      nextResult = storyIterator.next(cache);
+
+      // And keep processing
+      continue;
+    }
+
+    // Otherwise this is not a context action
+    // use the parent processor to process it.
+    const actionValue = yield nextResult.value;
+
+    // and pass that value down to our children.
+    nextResult = storyIterator.next(actionValue);
+  }
+
+  // Return the successful final result of the generator
+  return nextResult.value;
 }
