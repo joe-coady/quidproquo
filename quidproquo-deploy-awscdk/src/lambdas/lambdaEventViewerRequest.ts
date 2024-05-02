ed2aa9d5c9cb7d573d0117d04df91977eb0b543f
Joe Coady <joecoady@gmail.com>
2024-05-02 19:47:06 +1000

    updated bot test for prerender

--- quidproquo-deploy-awscdk/src/lambdas/lambdaEventViewerRequest.ts
+++ quidproquo-deploy-awscdk/src/lambdas/lambdaEventViewerRequest.ts
@@ -12,7 +12,13 @@ export const viewerRequestEventHandler = async (
 
   const userAgent = headers['user-agent'][0]?.value || 'unknown';
 
-  let isBot = userAgent.match(/bot|crawl|spider|slurp|facebot|facebookexternalhit/i);
+  // let isBot = userAgent.match(/bot|crawl|spider|slurp|facebot|facebookexternalhit/i);
+
+  // Bot testing for integrating Prerender ~ you then do what you want inside the app business logic
+  // For more info: https://docs.prerender.io/docs/apache-2
+  let isBot = userAgent.match(
+    /googlebot|bingbot|yandex|baiduspider|facebookexternalhit|twitterbot|rogerbot|linkedinbot|embedly|quora link preview|showyoubot|outbrain|pinterest\/0\.|pinterestbot|slackbot|vkShare|W3C_Validator|whatsapp|redditbot|applebot|flipboard|tumblr|bitlybot|skypeuripreview|nuzzel|discordbot|google page speed|qwantify|bitrix link preview|xing-contenttabreceiver|google-inspectiontool|chrome-lighthouse|telegrambot/i,
+  );
 
   return {
     ...request,
