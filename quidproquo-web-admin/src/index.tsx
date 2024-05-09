2f92c73b8ba240e692734762e771fa27962f6aee
Joe Coady <joecoady@gmail.com>
2024-05-09 13:16:27 +1000

    web-admin react strict mode

--- quidproquo-web-admin/src/index.tsx
+++ quidproquo-web-admin/src/index.tsx
@@ -1,7 +1,13 @@
+import { StrictMode } from 'react';
+
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 
 const domNode = document.getElementById('root');
 const root = createRoot(domNode!);
 
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
