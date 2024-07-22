5fc4ea43b24eb4caf1eca07ffda6a4bd180f4868
Joe Coady <joecoady@gmail.com>
2024-07-22 13:23:09 +1000

    bug fix dynamo partial data

--- quidproquo-core/src/stories/kvs/askKeyValueStoreUpdatePartialProperties.ts
+++ quidproquo-core/src/stories/kvs/askKeyValueStoreUpdatePartialProperties.ts
@@ -1,40 +1,47 @@
-import {
-  KvsUpdateAction,
-  askKeyValueStoreUpdate,
-  kvsRemove,
-  kvsSet,
-  kvsUpdate,
-} from '../../actions';
+import { KvsCoreDataType, KvsUpdateAction, askKeyValueStoreUpdate, kvsRemove, kvsSet, kvsUpdate } from '../../actions';
 import { AskResponse } from '../../types';
 import { isValidKvsAdvancedDataType } from './utils';
 
-export function* askKeyValueStoreUpdatePartialProperties<
+export function askKeyValueStoreUpdatePartialProperties<TModel extends Record<string, any>, PartitionKey extends keyof TModel>(
+  keyValueStoreName: string,
+  partitionKeyName: PartitionKey,
+  partialProperties: Partial<TModel> & { [key in PartitionKey]: TModel[PartitionKey] },
+): AskResponse<TModel>;
+
+export function askKeyValueStoreUpdatePartialProperties<
   TModel extends Record<string, any>,
-  TKey extends keyof TModel,
+  PartitionKey extends keyof TModel,
+  SortKey extends keyof TModel,
 >(
   keyValueStoreName: string,
-  partitionKeyName: TKey,
+  partitionKeyName: PartitionKey,
+  partialProperties: Partial<TModel> & { [key in PartitionKey]: TModel[PartitionKey] } & { [key in SortKey]: TModel[SortKey] },
+  sortKeyName: SortKey,
+): AskResponse<TModel>;
+
+export function* askKeyValueStoreUpdatePartialProperties<TModel extends Record<string, any>, PartitionKey extends keyof TModel>(
+  keyValueStoreName: string,
+  partitionKeyName: PartitionKey,
   partialProperties: Partial<TModel> & {
-    [key in TKey]: TModel[TKey];
+    [key in PartitionKey]: TModel[PartitionKey];
   },
+  sortKeyName?: keyof TModel,
 ): AskResponse<TModel> {
-  const operations = Object.entries(partialProperties).reduce<KvsUpdateAction[]>(
-    (acc, [key, value]) => {
-      if (key !== partitionKeyName) {
-        if (value === undefined) {
-          acc.push(kvsRemove(key));
-        } else if (isValidKvsAdvancedDataType(value)) {
-          acc.push(kvsSet(key, value));
-        }
+  const operations = Object.entries(partialProperties).reduce<KvsUpdateAction[]>((acc, [key, value]) => {
+    if (key !== partitionKeyName && key !== sortKeyName) {
+      if (value === undefined) {
+        acc.push(kvsRemove(key));
+      } else if (isValidKvsAdvancedDataType(value)) {
+        acc.push(kvsSet(key, value));
       }
-      return acc;
-    },
-    [],
-  );
+    }
+    return acc;
+  }, []);
 
   return yield* askKeyValueStoreUpdate<TModel>(
     keyValueStoreName,
     kvsUpdate(operations),
     partialProperties[partitionKeyName],
+    sortKeyName ? partialProperties[sortKeyName] : undefined,
   );
 }
