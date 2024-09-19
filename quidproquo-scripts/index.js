bc86430d01c2d9457483b122d1ad2abe72548949
Joe Coady <joecoady@gmail.com>
2024-09-19 11:30:21 +1000

    added qpq scripts base package

---
+++ quidproquo-scripts/index.js
@@ -0,0 +1,2 @@
+const args = process.argv.slice(2);
+console.log(`Argument passed: ${args.join(' ')}`);
---
+++ quidproquo-scripts/src/index.ts
@@ -0,0 +1,3 @@
+const args = process.argv.slice(2);
+
+console.log(`Argument passed: ${args.join(' ')}`);
