2737c02c555b060cc697f846544e4e7d6669fcbd
Joe Coady <joecoady@gmail.com>
2024-10-22 16:58:51 +1000

    wordWrap has to be const?

--- quidproquo-web-admin/src/LogViewer/actionComponents/genericActionRenderer/AnyVariableView/AnyVariableView.tsx
+++ quidproquo-web-admin/src/LogViewer/actionComponents/genericActionRenderer/AnyVariableView/AnyVariableView.tsx
@@ -8,7 +8,7 @@ export const genericFunctionRendererStyles = {
     padding: '10px', // Adding some padding for visual spacing
     borderRadius: '5px', // Optional: adds rounded corners to the <pre> tag
     whiteSpace: 'pre-wrap',
-    wordWrap: 'break-word',
+    wordWrap: 'break-word' as const,
   },
   functionName: { color: '#D0DC8B' }, // Function color
   stringValue: { color: '#CE834A' }, // String value color
