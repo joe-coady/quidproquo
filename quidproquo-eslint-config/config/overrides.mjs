ceb9689dcd5f33d3624d1be35b7eac827e48a57e
Joe Coady <joecoady@gmail.com>
2024-10-22 15:11:03 +1000

    disable require-yield

--- quidproquo-eslint-config/config/overrides.mjs
+++ quidproquo-eslint-config/config/overrides.mjs
@@ -1,10 +1,16 @@
 export const overrides = {
   rules: {
+    // Generators
+    'require-yield': 'off',
+
+    // Dev Exp
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
     'prefer-const': 'off',
 
+    // Probably remove
     '@typescript-eslint/no-unnecessary-type-arguments': 'off',
     '@typescript-eslint/no-unnecessary-generics': 'off',
+    '@typescript-eslint/no-empty-object-type': 'off',
   },
 };
