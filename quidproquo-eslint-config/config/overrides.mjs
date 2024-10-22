94880155e80cbddbf55466d068b4fc59ac8ab373
Joe Coady <joecoady@gmail.com>
2024-10-22 14:19:39 +1000

    updated lint settings

--- quidproquo-eslint-config/config/overrides.mjs
+++ quidproquo-eslint-config/config/overrides.mjs
@@ -3,5 +3,8 @@ export const overrides = {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
     'prefer-const': 'off',
+
+    '@typescript-eslint/no-unnecessary-type-arguments': 'off',
+    '@typescript-eslint/no-unnecessary-generics': 'off',
   },
 };
--- quidproquo-eslint-config/eslint.config.mjs
+++ quidproquo-eslint-config/eslint.config.mjs
@@ -4,6 +4,7 @@ import tseslint from 'typescript-eslint';
 import { overrides } from './config/overrides.mjs';
 
 export default [
+  { ignores: ['dist/', 'lib/'] },
   { files: ['**/*.{js,mjs,cjs,ts}'] },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
