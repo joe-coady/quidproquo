1aca3448cb6dee780be037aeaf4d4e2e557056ab
Joe Coady <joecoady@gmail.com>
2024-10-23 16:27:33 +1000

    added eslint-plugin-simple-import-sort to imports

--- quidproquo-eslint-config/eslint.config.mjs
+++ quidproquo-eslint-config/eslint.config.mjs
@@ -1,6 +1,8 @@
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import globals from 'globals';
-import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import pluginJs from '@eslint/js';
+
 import { overrides } from './config/overrides.mjs';
 
 export default [
@@ -9,6 +11,45 @@ export default [
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            [
+              '^\\u0000[^.]',
+              // Packages. `react`\`react-dom` packages come first.
+              '^react(-dom)?$',
+              '^prop-types',
+              // Internal packages come last.
+              '^\\w',
+              '^@',
+            ],
+            [
+              // aliases first
+              '^[~]',
+              '^fixtures',
+              // Parent imports. Put `..` last.
+              '^\\.\\.(?!/?$)',
+              '^\\.\\./?$',
+              // Other relative imports. Put same-folder imports and `.` last.
+              '^\\./(?=.*/)(?!/?$)',
+              '^\\.(?!/?$)',
+              '^\\./?$',
+              // With Side effect imports
+              '^\\u0000\\.',
+            ],
+          ],
+        },
+      ],
+      'simple-import-sort/exports': 'error',
+    },
+  },
 
   overrides,
 ];
