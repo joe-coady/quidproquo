701e2904494c0f19773bbe0069674df9d9778b89
Joe Coady <joecoady@gmail.com>
2026-03-18 22:16:22 +1000

    service requests can return data via a promise

--- quidproquo-webserver/src/actions/service/serviceRequest.ts
+++ quidproquo-webserver/src/actions/service/serviceRequest.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import {
   AskResponse,
   QueueEvent,
@@ -6,18 +5,38 @@ import {
   QueueMessage,
 } from 'quidproquo-core';
 
+import { askWebsocketReadConnectionInfo } from '../../context';
+import { askSendAnyWebSocketQueueEventMessageWithCorrelationToFrontend } from '../../services/webSocketQueue/logic/webSocket/askSendAnyWebSocketQueueEventMessageWithCorrelationToFrontend';
+import { WebSocketQueueServerEventMessageServiceRequestResponse } from '../../services/webSocketQueue/types/serverMessages/WebSocketQueueServerEventMessageServiceRequestResponse';
+import { WebSocketQueueServerMessageEventType } from '../../services/webSocketQueue/types/serverMessages/WebSocketQueueServerMessageEventType';
 import { ServiceRequester } from './createServiceRequester';
 
-export const serviceRequest = (
-  requester: ServiceRequester<any>,
-  runtime: (
-    event: QueueEvent<QueueMessage<any>>
-  ) => AskResponse<QueueEventResponse>
+type PayloadOf<R> = R extends ServiceRequester<infer T, any> ? T : never;
+type ResponseOf<R> = R extends ServiceRequester<any, infer T> ? T : never;
+
+export const serviceRequest = <R extends ServiceRequester<any, any>>(
+  requester: R,
+  runtime: (payload: PayloadOf<R>) => AskResponse<ResponseOf<R>>
 ) => {
   const { method } = requester.serviceRequest;
 
   const wrapper = function* wrapper(event: QueueEvent<QueueMessage<any>>) {
-    return yield* runtime(event);
+    const result = yield* runtime(event.message.payload);
+
+    const { connectionId, correlationId } =
+      yield* askWebsocketReadConnectionInfo();
+
+    const response: WebSocketQueueServerEventMessageServiceRequestResponse = {
+      type: WebSocketQueueServerMessageEventType.ServiceRequestResponse,
+      payload: result,
+    };
+
+    yield* askSendAnyWebSocketQueueEventMessageWithCorrelationToFrontend(
+      { ...response, correlationId },
+      connectionId,
+    );
+
+    return true as QueueEventResponse;
   };
 
   wrapper.serviceRequest = { method };
