6953591d7d5eab669d8a6553bc4b14f7e43b041e
Joe Coady <joecoady@gmail.com>
2026-04-19 17:18:30 +1000

    drop mime-types dep, inline lookup in core

--- quidproquo-core/src/utils/index.ts
+++ quidproquo-core/src/utils/index.ts
@@ -2,5 +2,6 @@ export * from './array';
 export * from './functions';
 export * from './hash';
 export * from './logs';
+export * from './mimeTypes';
 export * from './path';
 export * from './types';
