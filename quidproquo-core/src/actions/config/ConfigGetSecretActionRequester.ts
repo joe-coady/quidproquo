6c556082444e95f030923dde2088787dcf954a86
Joe Coady <joecoady@gmail.com>
2026-05-27 14:16:05 +1000

    error-handling pattern for config get secret

--- quidproquo-core/src/actions/config/ConfigGetSecretActionRequester.ts
+++ quidproquo-core/src/actions/config/ConfigGetSecretActionRequester.ts
@@ -1,5 +1,11 @@
+import { createErrorEnumForAction } from '../../types';
 import { ConfigActionType } from './ConfigActionType';
 import { ConfigGetSecretActionRequester } from './ConfigGetSecretActionTypes';
+
+export const ConfigGetSecretErrorTypeEnum = createErrorEnumForAction(ConfigActionType.GetSecret, [
+  'ResourceNotFound',
+  'Throttling',
+]);
 
 export function* askConfigGetSecret(secretName: string): ConfigGetSecretActionRequester {
   return yield { type: ConfigActionType.GetSecret, payload: { secretName } };
