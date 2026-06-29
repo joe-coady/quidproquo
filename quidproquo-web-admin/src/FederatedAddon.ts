10171ae45f776071fd6a0c8969aa50a20feae265
Joe Coady <joecoady@gmail.com>
2026-06-29 15:45:06 +1000

    web-admin: host provides federated addon loader

--- quidproquo-web-admin/src/FederatedAddon.ts
+++ quidproquo-web-admin/src/FederatedAddon.ts
@@ -1,3 +1,5 @@
+import { BaseUrlResolvers } from 'quidproquo-web-react';
+
 export type FederatedTab = {
   name: string;
   View: React.ComponentType;
@@ -10,3 +12,14 @@ export type FederatedAddon = {
   // We use this to detect what is an addon when importing from a federated module
   isQpqAdminFederatedAddon: boolean;
 };
+
+// Context handed to the addon loader so the host application can resolve
+// remote modules however it likes (eg. module federation, dynamic import, ...).
+export type LoadFederatedAddonsContext = {
+  baseUrlResolvers: BaseUrlResolvers;
+  accessToken?: string;
+};
+
+// Provided by the host application via <App loadAddons={...} />. quidproquo-web-admin
+// no longer owns the federation mechanism - it just asks for the addons.
+export type LoadFederatedAddons = (context: LoadFederatedAddonsContext) => Promise<FederatedAddon[]>;
